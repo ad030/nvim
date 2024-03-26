987814e860fe3ca9718c43c69926fd272b34c649
ad030 <alexdam030@gmail.com>
2024-03-26 15:04:57 -0400

    conform now uses config set up

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,24 +1,54 @@
 return {
 	{ -- Autoformat
 		"stevearc/conform.nvim",
-		opts = {
-			notify_on_error = false,
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
-			formatters_by_ft = {
-				lua = { "stylua" },
+		config = function()
+			local conform = require("conform")
 
-				java = { "google-java-format" },
+			conform.setup({
 
-				-- Conform can also run multiple formatters sequentially
-				python = { "isort", "black" },
-				--
-				-- You can use a sub-list to tell conform to run *until* a formatter
-				-- is found.
-				javascript = { { "prettierd", "prettier" } },
-			},
-		},
+				notify_on_error = false,
+				format_on_save = {
+					timeout_ms = 500,
+					lsp_fallback = true,
+					async = false,
+				},
+				formatters_by_ft = {
+					lua = { "stylua" },
+
+					java = { "google-java-format" },
+
+					-- Conform can also run multiple formatters sequentially
+					python = { "isort", "black" },
+					--
+					-- You can use a sub-list to tell conform to run *until* a formatter
+					-- is found.
+					javascript = { "prettier" },
+
+					typescript = { "prettier" },
+
+					javascriptreact = { "prettier" },
+
+					typescriptreact = { "prettier" },
+
+					css = { "prettier" },
+
+					html = { "prettier" },
+
+					json = { "prettier" },
+
+					yaml = { "prettier" },
+
+					markdown = { "prettier" },
+
+					graphql = { "prettier" },
+
+					svelte = { "prettier" },
+
+					xml = { "xmllint" },
+
+					csharp = { "astyle" },
+				},
+			})
+		end,
 	},
 }
