d406a464e50532f93242ed4f4fc219c0577351a6
ad030 <alexdam030@gmail.com>
2024-03-26 13:40:45 -0400

    added xml to treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,7 +3,19 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		opts = {
-			ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc", "java", "python", "c_sharp" },
+			ensure_installed = {
+				"bash",
+				"c",
+				"html",
+				"lua",
+				"markdown",
+				"vim",
+				"vimdoc",
+				"java",
+				"python",
+				"c_sharp",
+				"xml",
+			},
 			-- Autoinstall languages that are not installed
 			auto_install = true,
 			highlight = {
