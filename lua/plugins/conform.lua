4322c429b24cd61ec59763095475a7a5ec2536ff
ad030 <alexdam030@gmail.com>
2024-03-29 13:05:11 -0400

    fixed formatting on save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,11 +4,13 @@ return {
 	opts = {
 
 		notify_on_error = false,
-		format_on_save = {
-			timeout_ms = 500,
-			lsp_fallback = true,
-			async = false,
-		},
+		format_on_save = function(bufnr)
+			return {
+				timeout_ms = 500,
+				lsp_fallback = true,
+				async = false,
+			}
+		end,
 		formatters_by_ft = {
 			lua = { "stylua" },
 
