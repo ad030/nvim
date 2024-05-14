1c7fe752cd8eb9519e134e22fca8eef2aff2fe0e
ad030 <alexdam030@gmail.com>
2024-05-13 21:06:36 -0400

    added keybindings

--- lua/plugins/nvim-jdtls.lua
+++ lua/plugins/nvim-jdtls.lua
@@ -1,4 +1,16 @@
 return {
 	"mfussenegger/nvim-jdtls",
 	ft = "java",
+	dependencies = {
+		"mfussenegger/nvim-dap",
+	},
+	config = function()
+		vim.keymap.set("n", "<leader>jtc", require("jdtls").test_class(), { desc = "Java Test Class" })
+		vim.keymap.set(
+			"n",
+			"<leader>jtm",
+			require("jdtls").test_nearest_method(),
+			{ desc = "Java Test Nearest Method" }
+		)
+	end,
 }
--- lua/plugins/vscode-java-test.lua
+++
@@ -1,3 +0,0 @@
-return {
-	{ "microsoft/vscode-java-test" },
-}
