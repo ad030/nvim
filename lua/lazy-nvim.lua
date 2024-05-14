702ff2b96db7114b981cba4501b5847e1a6550b3
ad030 <alexdam030@gmail.com>
2024-05-13 21:21:55 -0400

    changed colorscheme

--- lua/lazy-nvim.lua
+++ lua/lazy-nvim.lua
@@ -1,18 +1,8 @@
 -- NOTE: Here is where you install your plugins.
 local plugins = {
-	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-	require("plugins/vim-sleuth"), -- Detect tabstop and shiftwidth automatically
+	require("colors.kanagawa"),
 
-	-- NOTE: Plugins can also be added by using a table,
-	-- with the first argument being the link and the following
-	-- keys can be used to configure plugin behavior/loading/etc.
-	--
-	-- Use `opts = {}` to force a plugin to be loaded.
-	--
-	--  This is equivalent to:
-	--    require('Comment').setup({})
-
-	-- "gc" to comment visual regions/lines
+	require("plugins/vim-sleuth"),
 
 	require("plugins.comment"),
 
@@ -35,8 +25,6 @@ local plugins = {
 	require("plugins.conform"),
 
 	require("plugins.cmp"),
-
-	require("plugins.tokyonight"),
 
 	require("plugins.todo-comments"),
 
