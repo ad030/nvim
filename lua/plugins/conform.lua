363151db9e189d69e4c8fa4d72624029976ea6fe
ad030 <68517956+ad030@users.noreply.github.com>
2024-06-10 15:49:52 -0400

    Update conform.lua

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -27,7 +27,7 @@ return {
 				--
 				-- You can use a sub-list to tell conform to run *until* a formatter
 				-- is found.
-				javascript = { { "prettierd", "prettier" } },
+				javascript = { "eslint_d", { "prettierd", "prettier" } },
 
 				typescript = { { "prettierd", "prettier" } },
 
